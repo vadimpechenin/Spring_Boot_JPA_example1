e0b47874100391c0e6b812fc5403be7a603cd77d
My Name <vadim.pechenin2015@gmail.com>
2020-11-25 23:25:34 +0300

    Registration and authorization

--- src/main/java/com/example/Spring_Boot_JPA/controller/MainController.java
+++ src/main/java/com/example/Spring_Boot_JPA/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.Spring_Boot_JPA;
+package com.example.Spring_Boot_JPA.controller;
 
 import com.example.Spring_Boot_JPA.domain.Message;
 import com.example.Spring_Boot_JPA.repos.MessageRepo;
@@ -10,19 +10,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired // его средства для получения bean-компонента под названием user Repository, который автоматически
     // создается Spring, мы будем использовать его для обработки данных
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false,
-            defaultValue="World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model){
         Iterable<Message> messages = messageRepo.findAll();
 
@@ -30,7 +29,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
 
